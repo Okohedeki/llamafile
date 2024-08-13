03bec6e5a661e48836d8b941e1a6969e7ec0e196
Okohedeki <okohedeki@gmail.com>
2024-08-13 00:50:12 -0700

    Update check_cpu.c
    This commit will break

--- llamafile/check_cpu.c
+++ llamafile/check_cpu.c
@@ -15,9 +15,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <cosmo.h>
-#include <errno.h>
-#include <stdlib.h>
 
 /**
  * Dies if CPU doesn't have mandatory features.
